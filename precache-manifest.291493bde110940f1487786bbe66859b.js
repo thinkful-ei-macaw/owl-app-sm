e764c80c6f6d6f64ce790bac1bffa5529061e01a
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-03 22:33:32 -0500

    Updates

---
+++ precache-manifest.291493bde110940f1487786bbe66859b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1af4dde5e5223b8f645b42b6a83baf",
+    "url": "/owl-app-sm/index.html"
+  },
+  {
+    "revision": "445a3cf321d2505f8779",
+    "url": "/owl-app-sm/static/css/main.b8b3d222.chunk.css"
+  },
+  {
+    "revision": "6b5e530de4d4af15c043",
+    "url": "/owl-app-sm/static/js/2.29706617.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/owl-app-sm/static/js/2.29706617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445a3cf321d2505f8779",
+    "url": "/owl-app-sm/static/js/main.e8440811.chunk.js"
+  },
+  {
+    "revision": "65c2db3ae668faa0e428",
+    "url": "/owl-app-sm/static/js/runtime-main.f0f7bb91.js"
+  }
+]);
